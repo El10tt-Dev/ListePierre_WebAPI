e5c0de9cd52ec58c0dfba1ecb4e33f832d2f7c42
Liban Eliott <e.liban@stag.technofuturtic.education>
2026-01-21 14:51:14 +0100

    ajout Entities pierre et propriétaire ajout scalar

---
+++ ListePierre/Data/Entities/Pierres.cs
@@ -0,0 +1,19 @@
+﻿namespace ListePierre.Data.Entities;
+
+public class Pierres : Proprietaires
+{
+    public Guid Id { get; set; }
+    public int IdProp { get; set; }
+    public string Nom { get; set; }
+    public string Origine { get; set; }
+    public double PoidsEnGramme { get; set; }
+    public string Description { get; set; }
+
+    public string DureteMesure { get; set; }
+    public DateOnly AcquisDepuis { get; set; }
+    public bool Exposee { get; set; } = false;
+
+
+
+
+}
---
+++ ListePierre/Data/Entities/Proprietaires.cs
@@ -0,0 +1,10 @@
+﻿namespace ListePierre.Data.Entities;
+
+public class Proprietaires
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Prenom { get; set; } = null;
+    public string Email { get; set; } = null!;
+    public int NombrePierrePossedee { get; set; } = 0;
+}
